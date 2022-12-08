44e9a950314e8f7fbafba47a853ef81709fa4378
Ильдар Ибрагимов <113426639+Ildar714717@users.noreply.github.com>
2022-12-08 18:51:09 +0300

    Create first_task.py

---
+++ homework1/first_task.py
@@ -0,0 +1,6 @@
+#lab1
+#first task
+name = str(input("Ваши фамилия, имя?"))
+age = int(input("Сколько вам лет?"))
+living_place = str(input("Где вы живете?"))
+print("Ваши фамилия, имя: ", name, "\n","Ваш возраст: ", age, "\n", "Вы живете в: ", living_place)
