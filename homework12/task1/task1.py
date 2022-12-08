a92241c31d5262cd20628c648e891f563cc8a93c
Ильдар Ибрагимов <113426639+Ildar714717@users.noreply.github.com>
2022-12-08 20:16:17 +0300

    Update task1.py

--- homework12/task1/task1.py
+++ homework12/task1/task1.py
@@ -1,5 +1,5 @@
 #lab12
 # task 1
-def remaind(x, y):
-    return remaind(x-y, y) if x >= y else x
+def remaind(a, b):
+    return remaind(a-b, b) if a >= b else a
 remaind(int(input("Введите первое число ")),int(input("Введите второе число ")))    
