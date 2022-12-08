6eeed361c53e3254bad107490a3799f76b3aef88
Ильдар Ибрагимов <113426639+Ildar714717@users.noreply.github.com>
2022-12-08 19:29:06 +0300

    Create task1.py

---
+++ homework3/task1/task1.py
@@ -0,0 +1,6 @@
+#lab 3
+#task 1
+def ThreeNumbers(a,b,c):
+  s = a+b+c
+  return(s)
+print("Сумма трех введенных чисел равна: ",ThreeNumbers(int(input("Введите первое число: ")),int(input("Введите второе число: ")),int(input("Введите третье число: "))))
