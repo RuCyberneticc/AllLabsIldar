aad214bac078f8340a6f38dcaf8f48884aca5759
Ильдар Ибрагимов <113426639+Ildar714717@users.noreply.github.com>
2022-12-15 14:17:45 +0300

    Delete homework6 directory

--- homework6/task_var9.py
+++
@@ -1,8 +0,0 @@
-#lab6
-def searcher(string, search):
- count = 0
- for i in range(len(string)):
-  if string[i: i + len(search)] == search:
-   count += 1
- return(count)
-searcher(str(input('Введите строку, в которой будем искать: \n')),search = str(input('Введите что искать: \n')))
